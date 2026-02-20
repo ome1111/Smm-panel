73bbbf4d683628f5a9b6eb1253e11fdc0d91d777
ome1111 <61970570+ome1111@users.noreply.github.com>
2026-02-20 12:57:25 +0300

    Update loader.py

--- loader.py
+++ loader.py
@@ -2,11 +2,17 @@ import telebot
 from pymongo import MongoClient
 from config import BOT_TOKEN, MONGO_URL
 
+# Bot Initializer
 bot = telebot.TeleBot(BOT_TOKEN, threaded=False)
+
+# Database Connection
 client = MongoClient(MONGO_URL)
 db = client['smm_database']
 
+# Collections
 users_col = db['users']
 orders_col = db['orders']
 config_col = db['settings']
-tickets_col = db['tickets'] # সাপোর্ট টিকিটের জন্য নতুন
+tickets_col = db['tickets']
+vouchers_col = db['vouchers']
+logs_col = db['logs']
